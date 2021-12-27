75c2b1b60613b8b1a290833428184a9790b63a82
Mars Wang <marswang0525@g.ncu.edu.tw>
2021-12-28 05:58:03 +0800

    first commit

--- linebot_template.py
+++ linebot_template.py
@@ -23,15 +23,17 @@ from linebot.models import (
     FlexSendMessage, VideoSendMessage,
 )
 from linebot.models.events import JoinEvent, PostbackEvent, MemberJoinedEvent, MemberLeftEvent, FollowEvent
-
+import logging
+import configparser
 
 app = Flask(__name__)
 
-#line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
-#handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
-line_bot_api = LineBotApi(
-    'h1FSu3wWcgB+6eTNYAXPA+vblQLpOGSoSBFW4T7d6kadGRzUHH6uG1NWcATbLjhWoL27+stVrdaX94+bjXJdWme36Upt34BMaZQ42E+flSWGDhHKodGGcIcPdUaSPDKtM5E8g1dGDg5BhpD0VkHZ1QdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('549b46ba8dad9b75950c6c05df709202')
+config = configparser.ConfigParser().read('config.ini')
+# Create your own config.ini file, and put your info in config.ini
+LINE_CHANNEL_ACCESS_TOKEN = config["linebot"]["line_bot_api"]
+LINE_Handler = config["linebot"]["handler"]
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_Handler)
 
 
 def getMessageObject(jsonObject):
@@ -60,18 +62,28 @@ def get_reply_messages(data):
     querystring_dict = dict(parse.parse_qsl(data))
     section = querystring_dict['section']
     action = querystring_dict['action']
+    try:
+        data = querystring_dict['data']
+    except:
+        data = "None"
     reply_table = pd.read_excel(
         'reply_messages_police.xlsx', engine='openpyxl')
     section_row = reply_table[reply_table.section == section]
     action_row = section_row[section_row.action == action]
+    print("action_row: ", action_row)
+    if data == "None":
+        data_row = action_row
+    else:
+        data_row = action_row[action_row.data == data]
+    print("Row: ", data_row)
     return_array = []
     for i in range(5):
-        if not pd.isna(action_row['message' + str(i + 1)].values[0]):
+        if not pd.isna(data_row['message' + str(i + 1)].values[0]):
             print("massage json string: ",
-                  action_row['message' + str(i + 1)].values[0])
-            print(type(action_row['message' + str(i + 1)].values[0]))
+                  data_row['message' + str(i + 1)].values[0])
+            print(type(data_row['message' + str(i + 1)].values[0]))
             jsonObject = json.loads(
-                action_row['message' + str(i + 1)].values[0])  # string to dict
+                data_row['message' + str(i + 1)].values[0])  # string to dict
             messageObject = getMessageObject(jsonObject)
             return_array.append(messageObject)
 
@@ -79,7 +91,7 @@ def get_reply_messages(data):
     return return_array
 
 
-@app.route("/", methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -108,13 +120,13 @@ def handle_message(event):
             event.reply_token, TextSendMessage(text="testing"))
     if msgtxt == "#replay":
         line_bot_api.reply_message(
-            event.reply_token, get_reply_messages("section=police&action=hello_asking"))
+            event.reply_token, get_reply_messages("section=jbwstudio&action=initial"))
 
 
 @handler.add(FollowEvent)
 def handle_follow(event):
     line_bot_api.reply_message(
-        event.reply_token, get_reply_messages("section=police&action=hello_asking"))
+        event.reply_token, get_reply_messages("section=jbwstudio&action=initial"))
 
 
 @handler.add(PostbackEvent)
@@ -127,4 +139,7 @@ def handle_postback(event):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.debug = True
+    loghandler = logging.FileHandler('flask.log')
+    app.logger.addHandler(loghandler)
+    app.run(port=5000)
